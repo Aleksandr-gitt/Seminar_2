eb19f578f2b13d903f3bdf1820dc21d52d1feb8e
Alex <sanyasan123@mail.ru>
2022-08-10 22:04:53 +0300

    Home_1

---
+++ Home_1/Program.cs
@@ -0,0 +1,23 @@
+﻿// Напишите программу, которая принимает на вход трёхзначное число 
+// и на выходе показывает вторую цифру этого числа.
+
+
+Console.WriteLine("Введите трехзначное число для получения результата");
+int enter = Convert.ToInt32(Console.ReadLine());
+if(enter < 0)
+{
+    enter = enter * -1;
+}
+    
+string qwerty = Convert.ToString(enter);
+int arg = Convert.ToInt32(qwerty.Length);
+    
+if(arg == 3)
+{
+    Console.WriteLine("Вторая цифра в Вашем числе - " + qwerty[1]);
+}
+else
+{
+    Console.WriteLine("Я же просил ввести !!!трехзначное!!! число.");
+}
+ 
