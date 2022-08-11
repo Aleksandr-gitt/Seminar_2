6ed0c50ce2dd38f0bb447a7ce1dcc333aa0b407e
Alex <sanyasan123@mail.ru>
2022-08-11 22:25:02 +0300

    chenge Home_2

--- Home_2/Program.cs
+++ Home_2/Program.cs
@@ -4,19 +4,30 @@
 Console.WriteLine("Введите Ваше число");
 int arg = Convert.ToInt32(Console.ReadLine());
 
-if(arg < 0)
-{
-    arg = arg * -1;
-}
+if(arg < 0) arg = arg * -1;
 
-int len = Convert.ToString(arg).Length;
-
-if(len > 2)
-{
-    string result = Convert.ToString(arg);
-    Console.WriteLine("Третье число в Вашей цифре - " + result[2]);
-}
-else
+if(arg < 100)
 {
     Console.WriteLine("В Вашем числе нет третьей цифры");
+    
+}
+else if(arg < 1000)
+{
+    Console.WriteLine("Третья цифра в Вашем числе - " + (arg % 10));
+}
+else 
+{
+    int n = 10;
+    int m = 1;
+    int result = arg / 10;
+
+    while(result > 0)
+    {
+        
+        result = result / 10;
+        n = n * 10;
+        m = m * 10;
+    }
+    
+    Console.WriteLine("Третья цифра в Вашем числе - " + (arg % (n/100) / (m/100)));
 }
