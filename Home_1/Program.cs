68695efdcba7820f2f7571bd46ec4801d78a91e7
Alex <sanyasan123@mail.ru>
2022-08-18 22:00:04 +0300

    Fin

--- Home_1/Program.cs
+++ Home_1/Program.cs
@@ -4,20 +4,10 @@
 
 Console.WriteLine("Введите трехзначное число для получения результата");
 int enter = Convert.ToInt32(Console.ReadLine());
-if(enter < 0)
-{
-    enter = enter * -1;
-}
-    
-string qwerty = Convert.ToString(enter);
-int arg = Convert.ToInt32(qwerty.Length);
-    
-if(arg == 3)
-{
-    Console.WriteLine("Вторая цифра в Вашем числе - " + qwerty[1]);
-}
-else
-{
-    Console.WriteLine("Я же просил ввести !!!трехзначное!!! число.");
-}
+if(enter < 0) enter = enter * -1;
+  
+int arg = enter / 10 % 10;
+  
+Console.WriteLine("Вторая цифра в Вашем числе - " + arg);
+
  
