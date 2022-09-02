9674aa8f5628879cdc639587045f181b0ed98a89
Aleksandr-gitt <109696041+Aleksandr-gitt@users.noreply.github.com>
2022-09-02 14:24:38 +0300

    Update Program.cs

--- Home_3/Program.cs
+++ Home_3/Program.cs
@@ -1,6 +1,12 @@
 ﻿// Напишите программу, которая принимает на вход цифру, обозначающую день недели,
 // и проверяет, является ли этот день выходным.
 
-Console.WriteLine("Введите порядковый номер дня недели, чтобы выяснить, является ли он выходным");
+Console.WriteLine("Введите число от 1 до 365");
 int number = Convert.ToInt32(Console.ReadLine());
 
+int n = number % 7;
+int m = number % 6;
+
+if(n == 0) Console.WriteLine(number + " день - Воскресенье");
+if(m == 0) Console.WriteLine(number + " день - Суббота");
+else Console.WriteLine(number + " день - Рабочий")
